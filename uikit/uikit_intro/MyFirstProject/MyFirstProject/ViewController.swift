fe2e49fa54484b43933ee4d8d42101536f37cebc
Hsin <itisjoe@gmail.com>
2016-05-11 23:29:39 +0800

    add comment

--- uikit/uikit_intro/MyFirstProject/MyFirstProject/ViewController.swift
+++ uikit/uikit_intro/MyFirstProject/MyFirstProject/ViewController.swift
@@ -12,15 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        // 定義一個 UIView 的常數 名稱為 firstView
         let firstView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
 
+        // 將 firstView 加入到 self.view
         self.view.addSubview(firstView)
 
+        // 取得螢幕的尺寸
         let fullScreenSize = UIScreen.mainScreen().bounds.size
 
+        // 設置 UIView 的位置到畫面的中心
         firstView.center = CGPoint(x: fullScreenSize.width * 0.5 , y: fullScreenSize.height * 0.5)
 
+        // 將 UIView 的底色設置為藍色
         firstView.backgroundColor = UIColor.blueColor()
 
     }
