5212b50f337269df73e08a75830ca207e383d8c3
Hsin <itisjoe@gmail.com>
2016-05-17 16:30:06 +0800

    add uiimageview

---
+++ uikit/uiimageview/ExAutoPlayImage/ExAutoPlayImage/ViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  ExAutoPlayImage
+//
+//  Created by joe feng on 2016/5/17.
+//  Copyright © 2016年 hsin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var myImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        // 取得螢幕的尺寸
+        let fullScreenSize = UIScreen.mainScreen().bounds.size
+        
+        myImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        
+        // 建立一個陣列 用來放置要輪播的圖片
+        let imgArr = [UIImage(named:"01.jpg")!,UIImage(named:"02.jpg")!,UIImage(named:"03.jpg")!]
+        
+        // 設置要輪播的圖片陣列
+        myImageView.animationImages = imgArr
+        
+        // 輪播一次的總秒數
+        myImageView.animationDuration = 6
+        
+        // 要輪播幾次 (設置 0 則為無限次)
+        myImageView.animationRepeatCount = 0
+        
+        // 開始輪播
+        myImageView.startAnimating()
+        
+        // 設置位置及放入畫面中
+        myImageView.center = CGPoint(x: fullScreenSize.width * 0.5, y: fullScreenSize.height * 0.4)
+        self.view.addSubview(myImageView)
+        
+        // 建立一個播放按鈕
+        let playButton = UIButton(frame: CGRect(x: 0, y: 0, width: 64, height: 64))
+        
+        // 設置播放按鈕的圖片
+        playButton.setImage(UIImage(named: "play"), forState: .Normal)
+        
+        // 設置按下播放按鈕的動作的方法
+        playButton.addTarget(self, action: #selector(ViewController.play), forControlEvents: .TouchUpInside)
+        
+        // 設置位置及放入畫面中
+        playButton.center = CGPoint(x: fullScreenSize.width * 0.35, y: fullScreenSize.height * 0.65)
+        self.view.addSubview(playButton)
+
+        // 建立一個停止按鈕
+        let stopButton = UIButton(frame: CGRect(x: 0, y: 0, width: 64, height: 64))
+        
+        // 設置停止按鈕的圖片
+        stopButton.setImage(UIImage(named: "stop"), forState: .Normal)
+        
+        // 設置按下停止按鈕的動作的方法
+        stopButton.addTarget(self, action: #selector(ViewController.stop), forControlEvents: .TouchUpInside)
+        
+        // 設置位置及放入畫面中
+        stopButton.center = CGPoint(x: fullScreenSize.width * 0.65, y: fullScreenSize.height * 0.65)
+        self.view.addSubview(stopButton)
+        
+    }
+    
+    func play() {
+        print("play images auto play")
+        myImageView.startAnimating()
+    }
+    
+    func stop() {
+        print("stop images auto play")
+        myImageView.stopAnimating()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ uikit/uiimageview/ExUIImageView/ExUIImageView/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  ExUIImageView
+//
+//  Created by joe feng on 2016/5/17.
+//  Copyright © 2016年 hsin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // 取得螢幕的尺寸
+        let fullScreenSize = UIScreen.mainScreen().bounds.size
+
+        // 使用 UIImageView(frame:) 建立一個 UIImageView
+        var myImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        
+        // 使用 UIImage(named:) 放置圖片檔案
+        myImageView.image = UIImage(named: "01.jpg")
+
+        // 設置新的位置並放入畫面中
+        myImageView.center = CGPoint(x: fullScreenSize.width * 0.5, y: fullScreenSize.height * 0.15)
+        self.view.addSubview(myImageView)
+
+        
+        // 使用 UIImageView(image:) 建立一個 UIImageView
+        myImageView = UIImageView(image: UIImage(named: "02.jpg"))
+        
+        // 設置原點及尺寸
+        myImageView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        
+        // 設置新的位置並放入畫面中
+        myImageView.center = CGPoint(x: fullScreenSize.width * 0.5, y: fullScreenSize.height * 0.35)
+        self.view.addSubview(myImageView)
+    
+        
+        // 使用 UIImageView(image:, highlightedImage:) 建立一個 UIImageView
+        myImageView = UIImageView(image: UIImage(named: "02.jpg"), highlightedImage: UIImage(named: "03.jpg"))
+        
+        // 設置原點及尺寸
+        myImageView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        
+        // 設置圖片 highlighted 狀態
+        myImageView.highlighted = true
+        
+        // 設置新的位置並放入畫面中
+        myImageView.center = CGPoint(x: fullScreenSize.width * 0.5, y: fullScreenSize.height * 0.55)
+        self.view.addSubview(myImageView)
+        
+
+        // 建立一個 UIImageView
+        myImageView = UIImageView(image: UIImage(named: "04.jpg"))
+        myImageView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+
+        // 設置背景顏色
+        myImageView.backgroundColor = UIColor.yellowColor()
+        
+        // 設置圖片顯示模式
+        myImageView.contentMode = .BottomLeft
+        
+        // 設置新的位置並放入畫面中
+        myImageView.center = CGPoint(x: fullScreenSize.width * 0.5, y: fullScreenSize.height * 0.8)
+        self.view.addSubview(myImageView)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
