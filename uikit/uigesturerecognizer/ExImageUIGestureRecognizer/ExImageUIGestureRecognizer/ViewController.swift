777ded82db6498a77bc79025a23f99f9185a19c2
Hsin <itisjoe@gmail.com>
2016-05-25 18:45:26 +0800

    add uigesturerecognizer

---
+++ uikit/uigesturerecognizer/ExImageUIGestureRecognizer/ExImageUIGestureRecognizer/ViewController.swift
@@ -0,0 +1,104 @@
+//
+//  ViewController.swift
+//  ExImageUIGestureRecognizer
+//
+//  Created by joe feng on 2016/5/25.
+//  Copyright © 2016年 hsin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var fullSize :CGSize!
+    var myImageView :UIImageView!
+    var anotherImageView :UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // 取得螢幕的尺寸
+        fullSize = UIScreen.mainScreen().bounds.size
+
+        
+        /************
+         
+         Pinch 縮放手勢
+         
+         ************/
+    
+        // 建立一個用來縮放的圖片
+        myImageView = UIImageView(image: UIImage(named: "01.jpg"))
+        myImageView.frame = CGRect(x: 50, y: 50, width: 200, height: 200)
+        self.view.addSubview(myImageView)
+        
+        let pinch = UIPinchGestureRecognizer(target:self,action:#selector(ViewController.pinch(_:)))
+
+        self.view.addGestureRecognizer(pinch)
+        
+        
+        /************
+         
+         Rotation 旋轉手勢
+         
+         ************/
+        
+        // 建立一個用來旋轉的圖片
+        anotherImageView = UIImageView(image: UIImage(named: "02.jpg"))
+        anotherImageView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        anotherImageView.center = CGPoint(x: fullSize.width * 0.5, y: fullSize.height * 0.75)
+        self.view.addSubview(anotherImageView)
+        
+        let rotation = UIRotationGestureRecognizer(target: self, action: #selector(ViewController.rotation(_:)))
+
+        self.view.addGestureRecognizer(rotation)
+
+    }
+
+    // 觸發旋轉手勢後 執行的動作
+    func rotation(recognizer:UIRotationGestureRecognizer) {
+        // 弧度
+        let radian = recognizer.rotation
+        
+        // 旋轉的弧度轉換為角度
+        let angle = radian * (180 / CGFloat(M_PI))
+        
+        anotherImageView.transform = CGAffineTransformMakeRotation(radian)
+        
+        print("旋轉角度： \(angle)")
+    }
+    
+    // 觸發縮放手勢後 執行的動作
+    func pinch(recognizer:UIPinchGestureRecognizer) {
+        if recognizer.state == .Began {
+            print("開始縮放")
+        } else if recognizer.state == .Changed {
+            // 圖片原尺寸
+            let frm = myImageView.frame
+            
+            // 縮放比例
+            let scale = recognizer.scale
+            
+            // 目前圖片寬度
+            let w = frm.width
+            
+            // 目前圖片高度
+            let h = frm.height
+            
+            // 縮放比例的限制為 0.5 ~ 2 倍
+            if w * scale > 100 && w * scale < 400 {
+                myImageView.frame = CGRect(x: frm.origin.x, y: frm.origin.y, width: w * scale, height: h * scale)
+            }
+        } else if recognizer.state == .Ended {
+            print("結束縮放")
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ uikit/uigesturerecognizer/ExUIGestureRecognizer/ExUIGestureRecognizer/ViewController.swift
@@ -0,0 +1,224 @@
+//
+//  ViewController.swift
+//  ExUIGestureRecognizer
+//
+//  Created by joe feng on 2016/5/25.
+//  Copyright © 2016年 hsin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var fullSize :CGSize!
+    var myUIView :UIView!
+    var anotherUIView :UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // 取得螢幕的尺寸
+        fullSize = UIScreen.mainScreen().bounds.size
+        
+        /************
+         
+         Tap 輕點手勢
+         
+         ************/
+        
+        // 雙指輕點 (雙指以上手勢只能用實機測試)
+        let doubleFingers = UITapGestureRecognizer(target:self,action:#selector(ViewController.doubleTap(_:)))
+        
+        // 點幾下才觸發 設置 1 時 則是要點一下才會觸發 依此類推
+        doubleFingers.numberOfTapsRequired = 1
+        
+        // 幾根指頭觸發
+        doubleFingers.numberOfTouchesRequired = 2
+
+        // 為視圖加入監聽手勢
+        self.view.addGestureRecognizer(doubleFingers)
+
+        
+        // 單指輕點
+        let singleFinger = UITapGestureRecognizer(target:self,action:#selector(ViewController.singleTap(_:)))
+
+        // 點幾下才觸發 設置 2 時 則是要點兩下才會觸發 依此類推
+        singleFinger.numberOfTapsRequired = 2
+        
+        // 幾根指頭觸發
+        singleFinger.numberOfTouchesRequired = 1
+        
+        // 雙指輕點沒有觸發時 才會檢測此手勢 以免手勢被蓋過
+        singleFinger.requireGestureRecognizerToFail(doubleFingers)
+        
+        // 為視圖加入監聽手勢
+        self.view.addGestureRecognizer(singleFinger)
+
+        
+        /************
+         
+         Long Press 長按手勢
+         
+         ************/
+        
+        // 長按
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longPress(_:)))
+        
+        // 為視圖加入監聽手勢
+        self.view.addGestureRecognizer(longPress)
+
+        
+        /************
+         
+         Swipe 滑動手勢
+         
+         ************/
+    
+        // 一個可供移動的 UIView
+        myUIView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        myUIView.backgroundColor = UIColor.blueColor()
+        self.view.addSubview(myUIView)
+        
+        // 向上滑動
+        let swipeUp = UISwipeGestureRecognizer(target:self, action:#selector(ViewController.swipe(_:)))
+        swipeUp.direction = .Up
+        
+        // 幾根指頭觸發 預設為 1
+        swipeUp.numberOfTouchesRequired = 1
+
+        // 為視圖加入監聽手勢
+        self.view.addGestureRecognizer(swipeUp)
+        
+        
+        // 向左滑動
+        let swipeLeft = UISwipeGestureRecognizer(target:self, action:#selector(ViewController.swipe(_:)))
+        swipeLeft.direction = .Left
+        
+        // 為視圖加入監聽手勢
+        self.view.addGestureRecognizer(swipeLeft)
+
+        
+        // 向下滑動
+        let swipeDown = UISwipeGestureRecognizer(target:self, action:#selector(ViewController.swipe(_:)))
+        swipeDown.direction = .Down
+        
+        // 為視圖加入監聽手勢
+        self.view.addGestureRecognizer(swipeDown)
+
+        
+        // 向右滑動
+        let swipeRight = UISwipeGestureRecognizer(target:self, action:#selector(ViewController.swipe(_:)))
+        swipeRight.direction = .Right
+
+        // 為視圖加入監聽手勢
+        self.view.addGestureRecognizer(swipeRight)
+        
+        
+        /************
+         
+         Pan 拖曳手勢
+         
+         ************/
+        
+        // 一個可供移動的 UIView
+        anotherUIView = UIView(frame: CGRect(x: fullSize.width * 0.5, y: fullSize.height * 0.5, width: 100, height: 100))
+        anotherUIView.backgroundColor = UIColor.orangeColor()
+        self.view.addSubview(anotherUIView)
+        
+        // 拖曳手勢
+        let pan = UIPanGestureRecognizer(target:self,action:#selector(ViewController.pan(_:)))
+        
+        // 最少可以用幾指拖曳
+        pan.minimumNumberOfTouches = 1
+        
+        // 最多可以用幾指拖曳
+        pan.maximumNumberOfTouches = 1
+        
+        // 為這個可移動的 UIView 加上監聽手勢
+        anotherUIView.addGestureRecognizer(pan)
+        
+    }
+    
+    // 觸發拖曳手勢後 執行的動作
+    func pan(recognizer:UIPanGestureRecognizer) {
+        // 設置 UIView 新的位置
+        let point = recognizer.locationInView(self.view)
+        anotherUIView.center = point
+    }
+    
+    // 觸發滑動手勢後 執行的動作
+    func swipe(recognizer:UISwipeGestureRecognizer) {
+        let point = myUIView.center
+
+        if recognizer.direction == .Up {
+            print("Go Up")
+            if point.y >= 150 {
+                myUIView.center = CGPoint(x: myUIView.center.x, y: myUIView.center.y - 100)
+            } else {
+                myUIView.center = CGPoint(x: myUIView.center.x, y: 50)
+            }
+        } else if recognizer.direction == .Left {
+            print("Go Left")
+            if point.x >= 150 {
+                myUIView.center = CGPoint(x: myUIView.center.x - 100, y: myUIView.center.y)
+            } else {
+                myUIView.center = CGPoint(x: 50, y: myUIView.center.y)
+            }
+        } else if recognizer.direction == .Down {
+            print("Go Down")
+            if point.y <= fullSize.height - 150 {
+                myUIView.center = CGPoint(x: myUIView.center.x, y: myUIView.center.y + 100)
+            } else {
+                myUIView.center = CGPoint(x: myUIView.center.x, y: fullSize.height - 50)
+            }
+        } else if recognizer.direction == .Right {
+            print("Go Right")
+            if point.x <= fullSize.width - 150 {
+                myUIView.center = CGPoint(x: myUIView.center.x + 100, y: myUIView.center.y)
+            } else {
+                myUIView.center = CGPoint(x: fullSize.width - 50, y: myUIView.center.y)
+            }
+        }
+    }
+    
+    // 觸發長按手勢後 執行的動作
+    func longPress(recognizer:UILongPressGestureRecognizer) {
+        if recognizer.state == .Began {
+            print("長按開始")
+        } else if recognizer.state == .Ended {
+            print("長按結束")
+        }
+        
+    }
+
+    // 觸發單指輕點兩下手勢後 執行的動作
+    func singleTap(recognizer:UITapGestureRecognizer){
+        print("單指連點兩下時觸發")
+        
+        // 取得每指的位置
+        self.findFingersPositon(recognizer)
+    }
+    
+    // 觸發雙指輕點一下手勢後 執行的動作
+    func doubleTap(recognizer:UITapGestureRecognizer){
+        print("雙指點一下時觸發")
+
+        // 取得每指的位置
+        self.findFingersPositon(recognizer)
+    }
+    
+    func findFingersPositon(recognizer:UITapGestureRecognizer) {
+        // 取得每指的位置
+        let number = recognizer.numberOfTouches()
+        for i in 0..<number {
+            let point = recognizer.locationOfTouch(i, inView: recognizer.view)
+            print("第 \(i + 1) 指的位置：\(NSStringFromCGPoint(point))")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
