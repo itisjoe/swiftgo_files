980846ad87608d0640d1ac706437caadfe1cd1bd
Hsin <itisjoe@gmail.com>
2016-06-14 18:06:45 +0800

    todo sound & settings

---
+++ apps/todo/ExSound/ExSound/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  ExSound
+//
+//  Created by joe feng on 2016/6/14.
+//  Copyright © 2016年 hsin. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    var myPlayer :AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 建立播放器
+        let soundPath = NSBundle.mainBundle().pathForResource("sound0132", ofType: "wav")
+        do {
+            myPlayer = try AVAudioPlayer(contentsOfURL: NSURL.fileURLWithPath(soundPath!))
+
+            // 重複播放次數 設為 0 則是只播放一次 不重複
+            myPlayer.numberOfLoops = 0
+        
+        } catch {
+            print("error")
+        }
+
+
+        // 建立一個按鈕
+        let myButton = UIButton(frame: CGRect(x: 100, y: 200, width: 100, height: 60))
+        myButton.setTitle("音效", forState: .Normal)
+        myButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        myButton.addTarget(self, action: #selector(ViewController.go), forControlEvents: .TouchUpInside)
+        self.view.addSubview(myButton)
+    
+    }
+
+    func go() {
+        // 播放音效
+        myPlayer.play()
+    }
+    
+}
+
---
+++ apps/todo/ToDo/ToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by joe feng on 2016/6/14.
+//  Copyright © 2016年 hsin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
