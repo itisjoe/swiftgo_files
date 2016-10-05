01329bfb07f53845446d9e13c7fe46a40236e7aa
Hsin <itisjoe@gmail.com>
2016-10-05 18:07:06 +0800

    swift3 ch2/nested-types

--- ch2/nested-types.playground/Contents.swift
+++ ch2/nested-types.playground/Contents.swift
@@ -2,29 +2,32 @@
 struct Poker {
     
     enum Suit: String {
-        case Spades = "黑桃", Hearts = "紅心", Diamonds = "方塊", Clubs = "梅花"
+        case spades = "黑桃", hearts = "紅心"
+        case diamonds = "方塊", clubs = "梅花"
     }
     
     enum Rank: Int {
-        case Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten
-        case Jack, Queen, King, Ace
+        case two = 2, three, four, five, six
+        case seven, eight, nine, ten
+        case jack, queen, king, ace
     }
     
     let rank: Rank, suit: Suit
     
     func description () {
-        print("這張牌的花色是：\(suit.rawValue)，點數為：\(rank.rawValue)")
+        print("這張牌的花色是：\(suit.rawValue)")
+        print("點數為：\(rank.rawValue)")
     }
     
 }
 
-let poker = Poker(rank: .King, suit: .Hearts)
+let poker = Poker(rank: .king, suit: .hearts)
 
 // 印出：這張牌的花色是：紅心，點數為：13
 poker.description()
 
 
-let diamondsName = Poker.Suit.Diamonds
+let diamondsName = Poker.Suit.diamonds
 
 // 印出：方塊
 print(diamondsName.rawValue)
