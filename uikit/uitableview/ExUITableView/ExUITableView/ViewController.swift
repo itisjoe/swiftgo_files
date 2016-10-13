c111c5c32b35e2f43cca2c48994f1697866b9407
Hsin <itisjoe@gmail.com>
2016-10-13 20:59:40 +0800

    swift3 uitableview

--- uikit/uitableview/ExUITableView/ExUITableView/ViewController.swift
+++ uikit/uitableview/ExUITableView/ExUITableView/ViewController.swift
@@ -18,20 +18,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
 
         // 取得螢幕的尺寸
-        let fullScreenSize = UIScreen.mainScreen().bounds.size
+        let fullScreenSize = UIScreen.main.bounds.size
         
         // 建立 UITableView 並設置原點及尺寸
-        let myTableView = UITableView(frame: CGRect(x: 0, y: 20, width: fullScreenSize.width, height: fullScreenSize.height - 20), style: .Grouped)
+        let myTableView = UITableView(frame: CGRect(x: 0, y: 20, width: fullScreenSize.width, height: fullScreenSize.height - 20), style: .grouped)
         
         // 註冊 cell 的樣式及名稱
-        myTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         
         // 設置委任對象
         myTableView.delegate = self
         myTableView.dataSource = self
         
         // 分隔線的樣式
-        myTableView.separatorStyle = .SingleLine
+        myTableView.separatorStyle = .singleLine
         
         // 分隔線的間距 四個數值分別代表 上、左、下、右 的間距
         myTableView.separatorInset = UIEdgeInsetsMake(0, 20, 0, 20)
@@ -48,25 +48,25 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     // 必須實作的方法：每一組有幾個 cell
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return info[section].count
     }
     
     // 必須實作的方法：每個 cell 要顯示的內容
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // 取得 tableView 目前使用的 cell
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
         
         // 設置 Accessory 按鈕樣式
         if indexPath.section == 1 {
             if indexPath.row == 0 {
-                cell.accessoryType = .Checkmark
+                cell.accessoryType = .checkmark
             } else if indexPath.row == 1 {
-                cell.accessoryType = .DetailButton
+                cell.accessoryType = .detailButton
             } else if indexPath.row == 2 {
-                cell.accessoryType = .DetailDisclosureButton
+                cell.accessoryType = .detailDisclosureButton
             } else if indexPath.row == 3 {
-                cell.accessoryType = .DisclosureIndicator
+                cell.accessoryType = .disclosureIndicator
             }
         }
         
@@ -79,29 +79,28 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
 
     // 點選 cell 後執行的動作
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // 取消 cell 的選取狀態
-        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true)
         
         let name = info[indexPath.section][indexPath.row]
         print("選擇的是 \(name)")
     }
-    
+
     // 點選 Accessory 按鈕後執行的動作
     // 必須設置 cell 的 accessoryType
-    // 設置為 .DisclosureIndicator (向右箭頭)之外都會觸發
-    func tableView(tableView: UITableView, accessoryButtonTappedForRowWithIndexPath indexPath: NSIndexPath) {
+    func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
         let name = info[indexPath.section][indexPath.row]
         print("按下的是 \(name) 的 detail")
     }
-    
+
     // 有幾組 section
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return info.count
     }
     
     // 每個 section 的標題
-    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         let title = section == 0 ? "籃球" : "棒球"
         return title
     }
@@ -109,33 +108,33 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 /*
     // 設置每個 section 的 title 為一個 UIView
     // 如果實作了這個方法 會蓋過單純設置文字的 section title
-    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         return UIView()
     }
-    
-    // 設置 section header 的高度 
-    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+
+    // 設置 section header 的高度
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 80
     }
-    
+
     // 每個 section 的 footer
-    func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
         return "footer"
     }
-    
+
     // 設置每個 section 的 footer 為一個 UIView
     // 如果實作了這個方法 會蓋過單純設置文字的 section footer
-    func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
         return UIView()
     }
-    
+
     // 設置 section footer 的高度
-    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         return 80
     }
-    
+
     // 設置 cell 的高度
-    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
     }
 */
@@ -144,7 +143,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
