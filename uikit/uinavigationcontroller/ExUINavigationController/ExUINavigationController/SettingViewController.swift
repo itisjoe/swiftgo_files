4a7d6f0018b5d7a736da02f6d674181665ceca8b
Hsin <itisjoe@gmail.com>
2016-10-17 12:16:38 +0800

    swift3 uinavigationcontroller

--- uikit/uinavigationcontroller/ExUINavigationController/ExUINavigationController/SettingViewController.swift
+++ uikit/uinavigationcontroller/ExUINavigationController/ExUINavigationController/SettingViewController.swift
@@ -14,35 +14,35 @@ class SettingViewController: UIViewController {
         super.viewDidLoad()
         
         // 底色
-        self.view.backgroundColor = UIColor.blackColor()
+        self.view.backgroundColor = UIColor.black
         
         // 導覽列標題
         self.title = "Setting"
         
         // 導覽列底色
-        self.navigationController?.navigationBar.barTintColor = UIColor.whiteColor()
+        self.navigationController!.navigationBar.barTintColor = UIColor.white
         
         // 導覽列是否半透明
-        self.navigationController?.navigationBar.translucent = false
+        self.navigationController!.navigationBar.isTranslucent = false
         
         // 導覽列右邊 UIView
         let myUIView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
-        myUIView.backgroundColor = UIColor.purpleColor()
+        myUIView.backgroundColor = UIColor.purple
         let rightButton = UIBarButtonItem(customView: myUIView)
         // 加到導覽列中
         self.navigationItem.rightBarButtonItem = rightButton
         
         // 建立一個按鈕
         let myButton = UIButton(frame: CGRect(x: 100, y: 250, width: 120, height: 40))
-        myButton.setTitle("回前頁", forState: .Normal)
-        myButton.backgroundColor = UIColor.blueColor()
-        myButton.addTarget(self, action: #selector(SettingViewController.back), forControlEvents: .TouchUpInside)
+        myButton.setTitle("回前頁", for: .normal)
+        myButton.backgroundColor = UIColor.blue
+        myButton.addTarget(self, action: #selector(SettingViewController.back), for: .touchUpInside)
         self.view.addSubview(myButton)
         
     }
 
     func back() {
-        self.navigationController?.popViewControllerAnimated(true)
+        self.navigationController!.popViewController(animated: true)
     }
 
     override func didReceiveMemoryWarning() {
--- uikit/uinavigationcontroller/ExUINavigationController/ExUINavigationController/ViewController.swift
+++ uikit/uinavigationcontroller/ExUINavigationController/ExUINavigationController/ViewController.swift
@@ -10,48 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewWillAppear(animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         // 如果導覽列底色在切換頁面後會改變
         // 可將設置底色放在這個方法裡
-        self.navigationController?.navigationBar.barTintColor = UIColor.lightGrayColor()
+        self.navigationController!.navigationBar.barTintColor = UIColor.lightGray
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // 底色
-        self.view.backgroundColor = UIColor.darkGrayColor()
+        self.view.backgroundColor = UIColor.darkGray
         
         // 導覽列標題
         self.title = "首頁"
         
         // 導覽列底色
-        self.navigationController?.navigationBar.barTintColor = UIColor.lightGrayColor()
+        self.navigationController!.navigationBar.barTintColor = UIColor.lightGray
         
         // 導覽列是否半透明
-        self.navigationController?.navigationBar.translucent = false
+        self.navigationController!.navigationBar.isTranslucent = false
         
         // 導覽列左邊按鈕
-        let leftButton = UIBarButtonItem(image: UIImage(named:"check"), style:.Plain , target:self , action: #selector(ViewController.check))
+        let leftButton = UIBarButtonItem(image: UIImage(named:"check"), style:.plain , target:self , action: #selector(ViewController.check))
         // 加到導覽列中
         self.navigationItem.leftBarButtonItem = leftButton
         
         // 導覽列右邊按鈕
-        let rightButton = UIBarButtonItem(title:"設定",style:.Plain ,target:self,action:#selector(ViewController.setting))
+        let rightButton = UIBarButtonItem(title:"設定",style:.plain ,target:self,action:#selector(ViewController.setting))
         // 加到導覽列中
         self.navigationItem.rightBarButtonItem = rightButton
         
         // 建立一個按鈕
         let myButton = UIButton(frame: CGRect(x: 0, y: 0, width: 120, height: 40))
-        myButton.setTitle("Article", forState: .Normal)
-        myButton.backgroundColor = UIColor.blueColor()
-        myButton.addTarget(self, action: #selector(ViewController.article), forControlEvents: .TouchUpInside)
+        myButton.setTitle("Article", for: .normal)
+        myButton.backgroundColor = UIColor.blue
+        myButton.addTarget(self, action: #selector(ViewController.article), for: .touchUpInside)
         self.view.addSubview(myButton)
 
     }
     
     func article() {
-        self.navigationController?.pushViewController(ArticleViewController(), animated: true)
+        self.navigationController!.pushViewController(ArticleViewController(), animated: true)
     }
     
     func check() {
@@ -59,7 +59,7 @@ class ViewController: UIViewController {
     }
     
     func setting() {
-        self.navigationController?.pushViewController(SettingViewController(), animated: true)
+        self.navigationController!.pushViewController(SettingViewController(), animated: true)
     }
     
     override func didReceiveMemoryWarning() {
