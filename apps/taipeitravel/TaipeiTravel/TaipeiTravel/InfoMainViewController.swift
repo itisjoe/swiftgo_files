c86c5309e2f4c58614b6d3ad5dc76ee3ba68de56
Hsin <itisjoe@gmail.com>
2017-12-06 16:51:24 +0800

    app[taipeitravel] update to Swift 4.0

--- apps/taipeitravel/TaipeiTravel/TaipeiTravel/InfoMainViewController.swift
+++ apps/taipeitravel/TaipeiTravel/TaipeiTravel/InfoMainViewController.swift
@@ -39,17 +39,17 @@ class InfoMainViewController: UIViewController, UITableViewDelegate, UITableView
         self.view.addSubview(self.myTableView)
     }
 
-    func goFB() {
+    @objc func goFB() {
         let requestUrl = URL(string: "https://www.facebook.com/1640636382849659")
         UIApplication.shared.open(requestUrl!, options: ["":""], completionHandler: nil)
     }
     
-    func goIconSource() {
+    @objc func goIconSource() {
         let requestUrl = URL(string: "http://www.flaticon.com/")
         UIApplication.shared.open(requestUrl!, options: ["":""], completionHandler: nil)
     }
 
-    func goDataSource() {
+    @objc func goDataSource() {
         let requestUrl = URL(string: "http://data.taipei/")
         UIApplication.shared.open(requestUrl!, options: ["":""], completionHandler: nil)
     }
